bb2de87f69249660826101d9a6925852704355cd
Áron <tandiaron@gmail.com>
2023-04-11 19:28:28 +0200

    creating tables

--- BL/DataSource/SQLiteDataSource.php
+++ BL/DataSource/SQLiteDataSource.php
@@ -26,11 +26,69 @@ class SQLiteDataSource implements IDataSource
      * @throws Exception - when couldn't connect to database
      */
     function __construct(string $dbpath) {
+
         try {
             // TODO: create db if not exists
-            $this->db = new SQLite3($dbpath, SQLITE3_OPEN_READWRITE);
+            $this->db = new SQLite3($dbpath);
+            $this->createTables();
         } catch (Exception $exception) {
             throw new Exception('Could not open database', 1, $exception);
+        }
+    }
+
+    public function createTables() {
+        $commands = ['CREATE TABLE IF NOT EXISTS main.Comment
+                        (
+                        Id       INTEGER                        not null
+                            primary key autoincrement
+                            unique,
+                        UserId   INTEGER                        not null,
+                        ShowId   INTEGER                        not null,
+                        Content  TEXT                           not null,
+                        DateTime TEXT default CURRENT_TIMESTAMP not null
+                        )',
+                    'CREATE TABLE IF NOT EXISTS main.Following
+                        (
+                        FollowerId INTEGER not null,
+                        FollowedId INTEGER not null,
+                        unique (FollowedId, FollowerId)
+                        )',
+                    'CREATE TABLE IF NOT EXISTS main.Show
+                        (
+                            Id          INTEGER           not null
+                                primary key autoincrement
+                                unique,
+                            Title       TEXT              not null,
+                            NumEpisodes INTEGER           not null,
+                            Description TEXT default NULL,
+                            CoverPath   TEXT default NULL not null,
+                            TrailerPath TEXT default NULL,
+                            OstPath     TEXT default NULL not null
+                        )',
+                        'CREATE TABLE IF NOT EXISTS main.User
+                        (
+                            Id                 INTEGER           not null
+                                primary key autoincrement
+                                unique,
+                            Username           TEXT              not null
+                                unique,
+                            Email              TEXT              not null
+                                unique,
+                            Password           TEXT              not null,
+                            ProfilePicturePath TEXT    default NULL,
+                            IsAdmin            INTEGER default 0 not null,
+                            CanComment         INTEGER default 1 not null
+                        )',
+                        'CREATE TABLE IF NOT EXISTS main.Watching
+                        (
+                            UserId   INTEGER not null,
+                            ShowId   INTEGER not null,
+                            Episodes INTEGER default NULL,
+                            Rating   INTEGER default NULL,
+                            unique (UserId, ShowId)
+                        )'];
+        foreach ($commands as $command) {
+            $this->db->exec($command);
         }
     }
 
