f5fbc2889cd61103fe7feda84bb62a029a7bc37a
Áron <tandiaron@gmail.com>
2023-04-12 23:49:28 +0200

    shows and search

--- shows.php
+++ shows.php
@@ -1,21 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>BingeVoyage | Sorozatok</title>
-    <link rel="stylesheet" href="Resources/src/css/style.css">
-    <link rel="icon" type="image/svg" href="Resources/src/img/logo-nobg.svg">
-</head>
-<body>
-<nav>
-    <ul class="navbar">
-        <li><a href="index.html">Főoldal</a></li>
-        <li><a class="active" href="shows.html">Sorozatok</a></li>
-        <li><a href="people.html">Emberek</a></li>
-        <li style="float:right"><a href="profile.html">Próba Ödön</a></li>
-        <li style="float:right"><a class="adminOnly" href="admin.html">Felületkezelés</a></li>
-    </ul>
-</nav>
+<?php
+
+use BL\DTO\_Interfaces\IRating;
+use BL\DTO\_Interfaces\IShow;
+
+session_start();
+
+$CURRENT_PAGE = 'shows';
+
+require 'Helpers/header.php';
+
+if (!isset($_GET['searchText'])) {
+    //TODO: error page
+    die('Oops1');
+}
+
+if (!isset($dataSource)) {
+    //TODO: error page
+    die('Oops2');
+}
+
+$showDao = $dataSource->createShowDAO();
+$ratingDao = $dataSource->createRatingDAO();
+$userDao = $dataSource->createRatingDAO();
+
+try {
+    $searchedShows = $showDao->getBySearchText($_GET['searchText']);
+    $shows = $showDao->getAll();
+} catch (Exception $e) {
+    die('Oops');
+}
+
+?>
 <main>
     <div class="searchBox">
         <form method="GET">
@@ -34,35 +49,29 @@
             <th id="rating">Értékelés</th>
             <th id="watching">Nézők</th>
         </tr>
-        <tr onclick="window.location.href = 'tmp/sites/shows/a_kiraly.html'">
-            <td headers="title"><img class="scalable" src="Resources/src/img/a_kiraly.jpg" alt="cover" width="100" height="100"></td>
-            <td headers="title" class="title">A Király</td>
-            <td headers="episodes">10</td>
-            <td headers="rating">3,8/5</td>
-            <td headers="watching">3</td>
+
+        <?php if ($_GET['searchText'] == null) {?>
+        <?php
+        /* @var $shows IShow */
+        foreach ($shows as $show) {
+        ?>
+        <tr onclick="window.location.href = 'shows.php?id=<?php echo $show->getId(); ?>'">
+            <td><img src="<?php echo $show->getCoverPath(); ?>" alt="cover" width="100" height="100"></td>
+            <td class="title"><?php echo $show->getTitle(); ?></td>
+            <td><?php echo $show->getNumEpisodes(); ?></td>
+            <td><?php echo $ratings = $ratingDao->getAverageRatingByShow($show); ?></td>
+            <td><?php echo count($ratings = $ratingDao->getByShow($show)); ?></td>
         </tr>
-        <tr onclick="window.location.href = 'tmp/sites/shows/mob_psycho_100.html'">
-            <td headers="title"><img class="scalable" src="Resources/src/img/mp100.jpg" alt="cover" width="100" height="100"></td>
-            <td headers="title" class="title">Mob Psycho 100</td>
-            <td headers="episodes">30</td>
-            <td headers="rating">4,7/5</td>
-            <td headers="watching">2</td>
-        </tr>
-        <tr onclick="window.location.href = 'tmp/sites/shows/cyberpunk_edgerunners.html'">
-            <td headers="title"><img class="scalable" src="Resources/src/img/cper.jpg" alt="cover" width="100" height="100"></td>
-            <td headers="title" class="title">Cyberpunk: Edgerunners</td>
-            <td headers="episodes">10</td>
-            <td headers="rating">4,3/5</td>
-            <td headers="watching">2</td>
-        </tr>
-        <tr onclick="window.location.href = 'tmp/sites/shows/hellsing-ultimate.html'">
-            <td headers="title"><img class="scalable" src="Resources/src/img/hsu.png" alt="cover" width="100" height="100"></td>
-            <td headers="title" class="title">Hellsing Ultimate</td>
-            <td headers="episodes">10</td>
-            <td headers="rating">4/5</td>
-            <td headers="watching">1</td>
-        </tr>
+        <?php } } else{
+            foreach ($searchedShows as $searchedShow) {
+            ?>
+            <tr onclick="window.location.href = 'shows.php?id=<?php echo $searchedShow->getId(); ?>'">
+                <td><img src="<?php echo $searchedShow->getCoverPath(); ?>" alt="cover" width="100" height="100"></td>
+                <td class="title"><?php echo $searchedShow->getTitle(); ?></td>
+                <td><?php echo $searchedShow->getNumEpisodes(); ?></td>
+                <td><?php echo $ratings = $ratingDao->getAverageRatingByShow($searchedShow); ?></td>
+                <td><?php echo count($ratings = $ratingDao->getByShow($searchedShow)); ?></td>
+            </tr>
+        <?php }} ?>
     </table>
 </main>
-</body>
-</html>
